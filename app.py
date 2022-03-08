7339ae900ac5d65c45d70195f1733fdfcf771be5
Quinn Conrad <quinnconrad15@gmail.com>
2022-03-08 13:04:44 -0600

    Add error message for invalid login

--- app.py
+++ app.py
@@ -208,9 +208,8 @@ def check():
         )
 
         if len(response['Items']) > 0:
-           return redirect('/')
-    return redirect('/login')
-
+            return redirect('/')
+    return render_template('login.html', error="Invalid username or password")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5001)
